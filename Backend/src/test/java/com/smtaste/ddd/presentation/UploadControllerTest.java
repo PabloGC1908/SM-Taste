270a4333cee304e107af462127ca7d6a0168efc1
LeonardOrme29 <leonardo.ormeno@unmsm.edu.pe>
2025-02-21 21:24:27 -0500

    test UploadControllerTest.java

---
+++ Backend/src/test/java/com/smtaste/ddd/presentation/UploadControllerTest.java
@@ -0,0 +1,36 @@
+package com.smtaste.ddd.presentation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UploadController.class)
+class UploadControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testUploadImage() throws Exception {
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "test.jpg",
+                MediaType.IMAGE_JPEG_VALUE,
+                "test image content".getBytes()
+        );
+
+        mockMvc.perform(multipart("/api/upload").file(file))
+                .andExpect(status().isOk())
+                .andExpect(content().string("http://localhost:8080/uploads/test.jpg"));
+    }
+}
